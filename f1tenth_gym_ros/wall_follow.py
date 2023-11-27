d4477d29cbeb0e166fbd370ad34101029e8af69a
CheesyChank <charleszhang418@hotmail.com>
2023-11-26 21:34:32 -0500

    Update wall_follow.py

--- f1tenth_gym_ros/wall_follow.py
+++ f1tenth_gym_ros/wall_follow.py
@@ -18,6 +18,24 @@ class WallFollow(Node):
         self.subscription_pose = self.create_subscription(Odometry, "/ego_racecar/odom", self.state_callback, 10)
         
 
+    """
+    float32 angle_min        # start angle of the scan [rad]
+    float32 angle_max        # end angle of the scan [rad]
+    float32 angle_increment  # angular distance between measurements [rad]
+
+    float32 time_increment   # time between measurements [seconds] - if your scanner
+                            # is moving, this will be used in interpolating position
+                            # of 3d points
+    float32 scan_time        # time between scans [seconds]
+
+    float32 range_min        # minimum range value [m]
+    float32 range_max        # maximum range value [m]
+
+    float32[] ranges         # range data [m] (Note: values < range_min or > range_max should be discarded)
+    float32[] intensities    # intensity data [device-specific units].  If your
+                            # device does not provide intensities, please leave
+                            # the array empty.
+    """
     # def scan_callback(self, msg):
     #     """Callback function for LaserScan messages. Print the range data."""
     #     ranges = msg.ranges
